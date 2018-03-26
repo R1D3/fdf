f252c02c14a4df4c3d69d90b9208468c6dbb1d5a
R1D3 <33871731+R1D3@users.noreply.github.com>
2018-03-26 09:58:43 +0200

    Update fdf.c

--- fdf.c
+++ fdf.c
@@ -14,8 +14,11 @@
 # include "fdf.h"
 
 void put_pixel(t_env *a, int tabxy[2])
-{   
-    a->imgstr[((a->x + a->y) / 2) * a->win_width + (a->x - a->y )] = WHITE;
+{
+    if (a->proj == 0)
+        a->imgstr[((a->x + a->y) / 2) * a->win_width + (a->x - a->y )] = WHITE;
+    else 
+        a->imgstr[a->y * a->win_width + a->x] = WHITE;
 }       
 
 void segment(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
@@ -113,8 +116,8 @@ void drawcoord(t_env *a)
                     }
                     else
                     {
-                        coord[tabxy[0]][tabxy[1]][0] = a->x1 - a->map[tabxy[0]][tabxy[1]] * 4;
-                        coord[tabxy[0]][tabxy[1]][1] = a->y1 - a->map[tabxy[0]][tabxy[1]] * 4;
+                        coord[tabxy[0]][tabxy[1]][0] = a->x1 - a->map[tabxy[0]][tabxy[1]] * a->mul;
+                        coord[tabxy[0]][tabxy[1]][1] = a->y1 - a->map[tabxy[0]][tabxy[1]] * a->mul;
                     }
                     a->x1 += a->prop;
                     tabxy[1]++;
@@ -197,14 +200,14 @@ int ft_reader(t_env *a)
 
 void	ft_space(t_env *fdf)
 {
-    fdf->propx = 420;
+    fdf->propx = 0;
     fdf->propy = 0;
 
 	if (fdf->chars <= 20)
 		fdf->prop = 30;
-	else if (fdf->chars >= 20 && fdf->chars < 50)
+	else if (fdf->chars >= 20 && fdf->chars <=50)
 		fdf->prop = 20;
-	else if (fdf->chars >= 50 && fdf->chars < 100)
+	else if (fdf->chars >= 50 && fdf->chars <= 100)
 		fdf->prop = 15;
 	else if (fdf->chars >= 100 && fdf->chars <= 200)
 		fdf->prop = 5;
@@ -233,9 +236,24 @@ void	ft_window(t_env *fdf)
 	else if (fdf->lines >= 50 && fdf->lines <= 100)
 		fdf->win_width = fdf->lines * 20;
 	else if (fdf->lines >= 100 && fdf->lines <= 200)
-		fdf->win_width = fdf->lines * 15;
+		fdf->win_width = fdf->lines * 20;
 	else
-		fdf->win_width = fdf->lines * 15;
+		fdf->win_width = fdf->lines * 20;
+}
+
+void init(t_env *a)
+{
+    if (a->proj == 1)
+    {
+        a->propx = 0;
+        a->propy = 0;
+        a->mul = 2;
+    }
+    a->propx = 340;
+    a->propy = -100;
+    a->mul = 4;
+    ft_space(a);
+    ft_window(a);
 }
 
 int	ft_expose_hook(t_env *a)
@@ -243,24 +261,42 @@ int	ft_expose_hook(t_env *a)
 	a->img = mlx_new_image(a->mlx, a->win_width, a->win_length);
 	a->imgstr = (int*)mlx_get_data_addr(a->img, &a->bpp, &a->sl, &a->endian);
 	drawcoord(a);
-	mlx_put_image_to_window(a->mlx, a->win, a->img, 0, 0);
 	mlx_destroy_image(a->mlx, a->img);
 	return (0);
 }
 
-void	ft_key_hook_proj(t_env *fdf)
+void	key_proj(t_env *fdf)
 {
 	if (fdf->proj == 0)
 		fdf->proj = 1;
 	else if (fdf->proj == 1)
 		fdf->proj = 0;
 }
-
-int key_funct(int keycode, t_env *a)
+void key_move2(int keycode, t_env *a)
 {
-    printf("%d\n", keycode );
-    if (keycode == 53)
-        exit(1);
+
+    if (keycode == 125)
+    {
+        a->propy += 20;
+    }
+    else if (keycode == 126)
+    {
+        a->propy -= 20;
+    }
+    else if (keycode == 124)
+    {
+        a->propx += 20;
+  
+    }
+    else if (keycode == 123)
+    {
+        a->propx -= 20;
+    }  
+}
+void key_move(int keycode, t_env *a)
+{
+    if (a->proj == 1)
+        key_move2(keycode, a);
     else if (keycode == 125)
     {
         a->propx += 20;
@@ -281,9 +317,48 @@ int key_funct(int keycode, t_env *a)
         a->propx -= 20;
         a->propy += 20;
     }
-    
-    printf("%d\n", a->propx);
-    printf("%d\n", a->propy);
+    printf("x = %d\n", a->propx);
+    printf("y = %d\n", a->propy);
+}
+
+void key_alt(int keycode, t_env *a)
+{
+    if (keycode == 12)
+        a->mul++;
+    if (keycode == 13)
+        a->mul--;
+}
+
+void key_zoom(int keycode, t_env *a)
+{
+    if (keycode == 24 && a->prop < 40)
+    {
+        a->prop += 5;
+        a->win_width += 200;
+        a->win_length += 200;
+    }
+    if (keycode == 27 && a->prop > 5)
+    {
+        a->prop -= 5;
+    }
+}
+
+int key_funct(int keycode, t_env *a)
+{
+    printf("%d\n", keycode );
+    if (keycode == 53)
+    {
+        exit(1);
+    }
+    else if (keycode == 124 || keycode == 125 || 
+            keycode == 126 || keycode == 123)
+            key_move(keycode, a);
+    else if (keycode == 24 || keycode == 27)
+            key_zoom(keycode, a);
+    else if (keycode == 49)
+        key_proj(a);
+    else if (keycode == 12 || keycode == 13)
+        key_alt(keycode, a);
     ft_expose_hook(a);
     return (0);
 }
@@ -291,20 +366,22 @@ int key_funct(int keycode, t_env *a)
 void mlx(t_env *a)
 {
 
-    ft_space(a);
-    ft_window(a);
+    init(a);
     a->mlx = mlx_init();
     a->win = mlx_new_window(a->mlx, a->win_width, a->win_length, "fdf42");
-    a->img = mlx_new_image(a->mlx, a->win_width, a->win_length);
+    a->img = mlx_new_image(a->mlx,a->win_width, a->win_length);
     a->imgstr = (int*)mlx_get_data_addr(a->img, &a->bpp, &a->sl, &a->endian);
     drawcoord(a);
     mlx_key_hook(a->win, key_funct, a);
+    mlx_expose_hook(a->win, ft_expose_hook, a);
     mlx_loop(a->mlx);
 }
 
 int main(int ac, char **av)
 {
 	t_env main;
+
+    ft_bzero(&main, sizeof(t_env));
 	if (ac == 2)
 	{
         main.file = av[1];
@@ -316,6 +393,8 @@ int main(int ac, char **av)
         if ((close(main.fd)) == -1)
 		    return (-1);
         mlx(&main);
+        free(main.map);
+        ft_bzero(&main, sizeof(t_env));
 	}
     else
 	    ft_putendl("usage : ./fdf file ");

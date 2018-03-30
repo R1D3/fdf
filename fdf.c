f4d0345b91b80fc53e145d33472d42dad7505bb4
R1D3 <33871731+R1D3@users.noreply.github.com>
2018-03-30 09:24:00 +0200

    Update fdf.c

--- fdf.c
+++ fdf.c
@@ -12,8 +12,9 @@
 
 
 # include "fdf.h"
+# include "fdf.h"
 
-void put_pixel(t_env *a, int tabxy[2])
+void put_pixel(t_env *a)
 {
     if (a->proj == 0)
         a->imgstr[((a->x + a->y) / 2) * a->win_width + (a->x - a->y )] = WHITE;
@@ -21,7 +22,7 @@ void put_pixel(t_env *a, int tabxy[2])
         a->imgstr[a->y * a->win_width + a->x] = WHITE;
 }       
 
-void segment(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
+void segment(t_env *a)
 {
     a->x = a->x1;
     a->y = a->y1;
@@ -31,7 +32,7 @@ void segment(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
     a->yinc = ( a->dy > 0 ) ? 1 : -1;
     a->dx = abs(a->dx);
     a->dy = abs(a->dy);
-    put_pixel(a,tabxy);
+    put_pixel(a);
     if (a->dx > a->dy)
     {
         a->cumul = a->dx / 2 ;
@@ -45,7 +46,7 @@ void segment(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
                 a->cumul -= a->dx;
                 a->y += a->yinc;
             }
-        put_pixel(a,tabxy);
+        put_pixel(a);
         }
     }
     else 
@@ -61,7 +62,7 @@ void segment(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
                 a->cumul -= a->dy ;
                 a->x += a->xinc ;
             }
-        put_pixel(a,tabxy);
+        put_pixel(a);
         }
     }
 }
@@ -69,10 +70,10 @@ void segment(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
 void draw(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
 {   
     tabxy[0] = 0;
-    while (a->y1 < a->win_length && tabxy[0] < a->lines)
+    while (tabxy[0] < a->lines)
         {
             tabxy[1] = 0;
-            while (a->x1 < a->win_width && tabxy[1] < a->chars)
+            while (tabxy[1] < a->chars)
                 {
                     a->x1 = coord[tabxy[0]][tabxy[1]][0];
                     a->y1 = coord[tabxy[0]][tabxy[1]][1];
@@ -80,13 +81,13 @@ void draw(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
                     {
                         a->x2 = coord[tabxy[0]][tabxy[1] + 1][0];
                         a->y2 = coord[tabxy[0]][tabxy[1] + 1][1];
-                        segment(a, coord, tabxy);
+                        segment(a);
                     }
                      if (tabxy[0] != (a->lines - 1))
                     {
                         a->x2 = coord[tabxy[0] + 1][tabxy[1]][0];
                         a->y2 = coord[tabxy[0] + 1][tabxy[1]][1];
-                        segment(a, coord, tabxy);
+                        segment(a);
                     }
                     tabxy[1]++;
                 }
@@ -102,12 +103,12 @@ void drawcoord(t_env *a)
 
     tabxy[0] = 0;
     a->y1 = a->propy;
-    while (a->y1 < a->win_length && tabxy[0] < a->lines)
+    while (tabxy[0] < a->lines)
         {
 
             a->x1 = a->propx;
             tabxy[1] = 0;
-            while (a->x1 < a->win_width && tabxy[1] < a->chars)
+            while (tabxy[1] < a->chars)
                 {
                     if (a->map[tabxy[0]][tabxy[1]] == 0)
                     {
@@ -116,6 +117,8 @@ void drawcoord(t_env *a)
                     }
                     else
                     {
+                        if (a->map[tabxy[0]][tabxy[1]] > 40 || a->map[tabxy[0]][tabxy[1]] < -40)
+                            a->mul = 2;
                         coord[tabxy[0]][tabxy[1]][0] = a->x1 - a->map[tabxy[0]][tabxy[1]] * a->mul;
                         coord[tabxy[0]][tabxy[1]][1] = a->y1 - a->map[tabxy[0]][tabxy[1]] * a->mul;
                     }
@@ -200,60 +203,87 @@ int ft_reader(t_env *a)
 
 void	ft_space(t_env *fdf)
 {
-    fdf->propx = 0;
-    fdf->propy = 0;
-
 	if (fdf->chars <= 20)
 		fdf->prop = 30;
-	else if (fdf->chars >= 20 && fdf->chars <=50)
+	else if (fdf->chars >= 20 && fdf->chars < 50)
 		fdf->prop = 20;
 	else if (fdf->chars >= 50 && fdf->chars <= 100)
-		fdf->prop = 15;
+		fdf->prop = 10;
 	else if (fdf->chars >= 100 && fdf->chars <= 200)
 		fdf->prop = 5;
 	else
 		fdf->prop = 1;
 }
 
+void	ft_pos(t_env *fdf)
+{
+    if (fdf->chars < 10)
+        {
+            fdf->propx = 200;
+            fdf->propy = 0;
+        }
+	else if (fdf->chars <= 20)
+		{
+            fdf->propx = 260;
+            fdf->propy = -120;
+        }
+	else if (fdf->chars >= 20 && fdf->chars < 50)
+		{
+            fdf->propx = 340;
+            fdf->propy = -100;
+        }
+	else if (fdf->chars >= 50 && fdf->chars <= 100)
+		{
+            fdf->propx = 480;
+            fdf->propy = -20;
+        }
+	else if (fdf->chars >= 100 && fdf->chars <= 200)
+		{
+            fdf->propx = 600;
+            fdf->propy = -200;
+        }
+}
+
 void	ft_window(t_env *fdf)
 {
-	if (fdf->chars <= 10)
-		fdf->win_length = fdf->chars * 100;
-	else if (fdf->chars <= 20)
-		fdf->win_length = fdf->chars * 70;
-	else if (fdf->chars >= 20 && fdf->chars <= 50)
-		fdf->win_length = fdf->chars * 40;
-	else if (fdf->chars >= 50 && fdf->chars <= 100)
-		fdf->win_length = fdf->chars * 20;
-	else if (fdf->chars >= 100 && fdf->chars <= 200)
-		fdf->win_length = fdf->chars * 10;
-	else
-		fdf->win_length = fdf->chars * 10;
-	if (fdf->lines <= 20)
-		fdf->win_width = fdf->lines * 100;
-	else if (fdf->lines >= 20 && fdf->lines <= 50)
-		fdf->win_width = fdf->lines * 40;
-	else if (fdf->lines >= 50 && fdf->lines <= 100)
-		fdf->win_width = fdf->lines * 20;
+	if (fdf->lines <= 10)
+		fdf->win_length = fdf->lines * 70;
+	else if (fdf->lines < 20)
+		fdf->win_length = fdf->lines * 50;
+	else if (fdf->lines >= 20 && fdf->lines < 50)
+		fdf->win_length = fdf->lines * 40;
+	else if (fdf->lines >= 50 && fdf->lines < 100)
+		fdf->win_length = fdf->lines * 20;
 	else if (fdf->lines >= 100 && fdf->lines <= 200)
-		fdf->win_width = fdf->lines * 20;
+		fdf->win_length = fdf->lines * 10;
 	else
-		fdf->win_width = fdf->lines * 20;
+		fdf->win_length = fdf->lines * 10;
+	if (fdf->chars <= 10)
+		fdf->win_width = fdf->chars * 100;
+    else if (fdf->chars < 20)
+		fdf->win_width = fdf->chars * 50;
+	else if (fdf->chars >= 20 && fdf->chars < 50)
+		fdf->win_width = fdf->chars * 40;
+	else if (fdf->chars >= 50 && fdf->chars < 100)
+		fdf->win_width = fdf->chars * 20;
+	else if (fdf->chars >= 100 && fdf->chars <= 200)
+		fdf->win_width = fdf->chars * 10;
+	else
+		fdf->win_width = fdf->chars * 20;
 }
 
 void init(t_env *a)
 {
     if (a->proj == 1)
     {
-        a->propx = 0;
-        a->propy = 0;
-        a->mul = 2;
+        a->propx = 150;
+        a->propy = 150;  
     }
-    a->propx = 340;
-    a->propy = -100;
-    a->mul = 4;
-    ft_space(a);
-    ft_window(a);
+    else
+    {
+        ft_pos(a);
+        a->mul = 4;
+    }
 }
 
 int	ft_expose_hook(t_env *a)
@@ -297,50 +327,61 @@ void key_move(int keycode, t_env *a)
 {
     if (a->proj == 1)
         key_move2(keycode, a);
-    else if (keycode == 125)
+    else if (keycode == 125 && (a->x < a->win_width || a->y < a->win_width
+        || a->x > -a->win_width || a->y > -a->win_width))
     {
         a->propx += 20;
         a->propy += 20;
     }
-    else if (keycode == 126)
+    else if (keycode == 126 && (a->x < a->win_width || a->y < a->win_width
+        || a->x > -a->win_width || a->y > -a->win_width))
     {
         a->propx -= 20;
         a->propy -= 20;
     }
-    else if (keycode == 124)
+    else if (keycode == 124 &&  (a->x < a->win_width || a->y < a->win_width
+        || a->x > -a->win_width || a->y > -a->win_width))
     {
         a->propx += 20;
         a->propy -= 20;
     }
-    else if (keycode == 123)
+    else if (keycode == 123 &&  (a->x < a->win_width || a->y < a->win_width
+        || a->x > -a->win_width || a->y > -a->win_width))
     {
         a->propx -= 20;
         a->propy += 20;
     }
     printf("x = %d\n", a->propx);
     printf("y = %d\n", a->propy);
+    printf("width = %d\n", a->win_width);
+    printf("length = %d\n", a->win_length);
 }
 
 void key_alt(int keycode, t_env *a)
 {
-    if (keycode == 12)
+    if (keycode == 12 &&  (a->x < a->win_width || a->y < a->win_width
+        || a->x > -a->win_width || a->y  > -a->win_width))
         a->mul++;
-    if (keycode == 13)
+    if (keycode == 13 &&  (a->x < a->win_width || a->y < a->win_width
+        || a->x >= -a->win_width || a->y > -a->win_width))
         a->mul--;
 }
 
 void key_zoom(int keycode, t_env *a)
 {
-    if (keycode == 24 && a->prop < 40)
+    if (keycode == 24 && (a->x < a->win_width || a->y < a->win_width
+        || a->x <= -a->win_width || a->y <= -a->win_width))
     {
         a->prop += 5;
-        a->win_width += 200;
-        a->win_length += 200;
     }
-    if (keycode == 27 && a->prop > 5)
+    if (keycode == 27)
     {
         a->prop -= 5;
+        if (a->prop <= 0)
+            a->prop = 5;
     }
+    printf(" x = %d\n", a->x);
+    printf("y = %d\n", a->y);
 }
 
 int key_funct(int keycode, t_env *a)
@@ -350,13 +391,16 @@ int key_funct(int keycode, t_env *a)
     {
         exit(1);
     }
-    else if (keycode == 124 || keycode == 125 || 
+    if (keycode == 124 || keycode == 125 || 
             keycode == 126 || keycode == 123)
             key_move(keycode, a);
     else if (keycode == 24 || keycode == 27)
             key_zoom(keycode, a);
     else if (keycode == 49)
+    {
         key_proj(a);
+        init(a);
+    }
     else if (keycode == 12 || keycode == 13)
         key_alt(keycode, a);
     ft_expose_hook(a);
@@ -365,8 +409,10 @@ int key_funct(int keycode, t_env *a)
 
 void mlx(t_env *a)
 {
-
+    a->proj = 0;
     init(a);
+    ft_space(a);
+    ft_window(a);
     a->mlx = mlx_init();
     a->win = mlx_new_window(a->mlx, a->win_width, a->win_length, "fdf42");
     a->img = mlx_new_image(a->mlx,a->win_width, a->win_length);
@@ -400,3 +446,4 @@ int main(int ac, char **av)
 	    ft_putendl("usage : ./fdf file ");
 	return (0);
 }
+

91db9df2d92ee7fe82ede4e73c839b97907fd92c
R1D3 <33871731+R1D3@users.noreply.github.com>
2018-04-05 08:45:58 +0200

    Update fdf.c

--- fdf.c
+++ fdf.c
@@ -12,55 +12,79 @@
 
 
 # include "fdf.h"
-# include "fdf.h"
 
 void put_pixel(t_env *a)
 {
-    if (a->proj == 0)
-        a->imgstr[((a->x + a->y) / 2) * a->win_width + (a->x - a->y )] = WHITE;
-    else 
-        a->imgstr[a->y * a->win_width + a->x] = WHITE;
+    printf(" xmax = %d\n", a->point.xmax);
+    printf("ymax = %d\n", a->point.ymax);
+    printf(" xmin = %d\n", a->point.xmin);
+    printf("ymin = %d\n", a->point.ymin);
+        if (a->proj == 0)
+            a->imgstr[((a->point.x + a->point.y) / 2) * a->win_width + (a->point.x - a->point.y )] = WHITE;
+        else 
+            if (a->point.x < a->win_width && a->point.x > -a->win_width &&
+            a->point.y < a->win_length && a->point.y > -a->win_length)
+                a->imgstr[a->point.y * a->win_width + a->point.x] = WHITE;
 }       
+
+void initcoord(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
+{
+    a->point.xmin = a->propx;
+    a->point.ymin = a->propy;
+    if (coord[tabxy[0]][tabxy[1]][0] > a->point.xmax &&
+         tabxy[0] < a->lines && tabxy[1] < a->chars)
+        a->point.xmax = coord[tabxy[0]][tabxy[1]][0];
+    if (coord[tabxy[0]][tabxy[1]][0] < a->point.xmin &&
+         tabxy[0] < a->lines && tabxy[1] < a->chars)
+        a->point.xmin = coord[tabxy[0]][tabxy[1]][0];
+    if (coord[tabxy[0]][tabxy[1]][1] > a->point.ymax &&
+         tabxy[0] < a->lines && tabxy[1] < a->chars)
+        a->point.ymax = coord[tabxy[0]][tabxy[1]][1];
+    if (coord[tabxy[0]][tabxy[1]][0] < a->point.xmin &&
+         tabxy[0] < a->lines && tabxy[1] < a->chars)
+        a->point.ymin = coord[tabxy[0]][tabxy[1]][1];
+}
 
 void segment(t_env *a)
 {
-    a->x = a->x1;
-    a->y = a->y1;
-    a->dx = (a->x2 - a->x1);
-    a->dy = (a->y2 - a->y1);
-    a->xinc = ( a->dx > 0 ) ? 1 : -1;
-    a->yinc = ( a->dy > 0 ) ? 1 : -1;
-    a->dx = abs(a->dx);
-    a->dy = abs(a->dy);
+    a->point.x = a->point.x1;
+    a->point.y = a->point.y1;
+    a->point.dx = (a->point.x2 - a->point.x1);
+    a->point.dy = (a->point.y2 - a->point.y1);
+    a->point.xinc = ( a->point.dx > 0 ) ? 1 : -1;
+    a->point.yinc = ( a->point.dy > 0 ) ? 1 : -1;
+    a->point.dx = abs(a->point.dx);
+    a->point.dy = abs(a->point.dy);
     put_pixel(a);
-    if (a->dx > a->dy)
+    if (a->point.dx > a->point.dy)
     {
-        a->cumul = a->dx / 2 ;
+        a->point.cumul = a->point.dx / 2 ;
         a->i = 1;
-        while (a->i++ <= a->dx)
+        while (a->i++ <= a->point.dx)
         {
-            a->x += a->xinc;
-            a->cumul += a->dy;
-            if (a->cumul >= a->dx)
+            a->point.x += a->point.xinc;
+            a->point.cumul += a->point.dy;
+            if (a->point.cumul >= a->point.dx)
             {
-                a->cumul -= a->dx;
-                a->y += a->yinc;
+                a->point.cumul -= a->point.dx;
+                a->point.y += a->point.yinc;
             }
         put_pixel(a);
+        
         }
     }
     else 
     {
-        a->cumul = a->dy / 2 ;
+        a->point.cumul = a->point.dy / 2 ;
         a->i = 1;
-        while (a->i++ <= a->dy)
+        while (a->i++ <= a->point.dy)
         {
-            a->y += a->yinc ;
-            a->cumul += a->dx ;
-            if ( a->cumul >= a->dy ) 
+            a->point.y += a->point.yinc ;
+            a->point.cumul += a->point.dx ;
+            if ( a->point.cumul >= a->point.dy ) 
             {
-                a->cumul -= a->dy ;
-                a->x += a->xinc ;
+                a->point.cumul -= a->point.dy ;
+                a->point.x += a->point.xinc ;
             }
         put_pixel(a);
         }
@@ -75,18 +99,18 @@ void draw(t_env *a, int coord[][a->chars + 1][2], int tabxy[2])
             tabxy[1] = 0;
             while (tabxy[1] < a->chars)
                 {
-                    a->x1 = coord[tabxy[0]][tabxy[1]][0];
-                    a->y1 = coord[tabxy[0]][tabxy[1]][1];
+                    a->point.x1 = coord[tabxy[0]][tabxy[1]][0];
+                    a->point.y1 = coord[tabxy[0]][tabxy[1]][1];
                     if (tabxy[1] != (a->chars - 1))
                     {
-                        a->x2 = coord[tabxy[0]][tabxy[1] + 1][0];
-                        a->y2 = coord[tabxy[0]][tabxy[1] + 1][1];
+                        a->point.x2 = coord[tabxy[0]][tabxy[1] + 1][0];
+                        a->point.y2 = coord[tabxy[0]][tabxy[1] + 1][1];
                         segment(a);
                     }
                      if (tabxy[0] != (a->lines - 1))
                     {
-                        a->x2 = coord[tabxy[0] + 1][tabxy[1]][0];
-                        a->y2 = coord[tabxy[0] + 1][tabxy[1]][1];
+                        a->point.x2 = coord[tabxy[0] + 1][tabxy[1]][0];
+                        a->point.y2 = coord[tabxy[0] + 1][tabxy[1]][1];
                         segment(a);
                     }
                     tabxy[1]++;
@@ -102,32 +126,31 @@ void drawcoord(t_env *a)
     int coord[a->lines + 1][a->chars + 1][2];
 
     tabxy[0] = 0;
-    a->y1 = a->propy;
+    a->point.y1 = a->propy;
     while (tabxy[0] < a->lines)
         {
-
-            a->x1 = a->propx;
+            a->point.x1 = a->propx;
             tabxy[1] = 0;
             while (tabxy[1] < a->chars)
                 {
                     if (a->map[tabxy[0]][tabxy[1]] == 0)
                     {
-                        coord[tabxy[0]][tabxy[1]][0] = a->x1;
-                        coord[tabxy[0]][tabxy[1]][1] = a->y1;
+                        coord[tabxy[0]][tabxy[1]][0] = a->point.x1;
+                        coord[tabxy[0]][tabxy[1]][1] = a->point.y1;
                     }
                     else
                     {
-                        if (a->map[tabxy[0]][tabxy[1]] > 40 || a->map[tabxy[0]][tabxy[1]] < -40)
-                            a->mul = 2;
-                        coord[tabxy[0]][tabxy[1]][0] = a->x1 - a->map[tabxy[0]][tabxy[1]] * a->mul;
-                        coord[tabxy[0]][tabxy[1]][1] = a->y1 - a->map[tabxy[0]][tabxy[1]] * a->mul;
+                        coord[tabxy[0]][tabxy[1]][0] = a->point.x1 - a->map[tabxy[0]][tabxy[1]] * a->mul;
+                        coord[tabxy[0]][tabxy[1]][1] = a->point.y1 - a->map[tabxy[0]][tabxy[1]] * a->mul;
                     }
-                    a->x1 += a->prop;
+                    initcoord(a, coord, tabxy);
+                    a->point.x1 += a->prop;
                     tabxy[1]++;
                 }
-            a->y1 += a->prop;
+            a->point.y1 += a->prop;
             tabxy[0]++;
         }
+    initcoord(a, coord, tabxy);
     draw(a, coord, tabxy);
 }
 
@@ -215,12 +238,46 @@ void	ft_space(t_env *fdf)
 		fdf->prop = 1;
 }
 
+void	ft_pos2(t_env *fdf)
+{
+    if (fdf->chars < 10)
+        {
+            fdf->propx = 10;
+            fdf->propy = 10;
+        }
+    else if (fdf->chars < 20)
+		{
+            fdf->propx = 0;
+            fdf->propy = 0;
+        }
+	else if (fdf->chars <= 20)
+		{
+            fdf->propx = 50;
+            fdf->propy = 50;
+        }
+	else if (fdf->chars >= 20 && fdf->chars < 50)
+		{
+            fdf->propx = 50;
+            fdf->propy = 50;
+        }
+	else if (fdf->chars >= 50 && fdf->chars <= 100)
+		{
+            fdf->propx = 100;
+            fdf->propy = 100;
+        }
+	else if (fdf->chars >= 100 && fdf->chars <= 200)
+		{
+            fdf->propx = 150;
+            fdf->propy = 150;
+        }
+}
+
 void	ft_pos(t_env *fdf)
 {
     if (fdf->chars < 10)
         {
-            fdf->propx = 200;
-            fdf->propy = 0;
+            fdf->propx = 100;
+            fdf->propy = 20;
         }
 	else if (fdf->chars <= 20)
 		{
@@ -276,22 +333,21 @@ void init(t_env *a)
 {
     if (a->proj == 1)
     {
-        a->propx = 150;
-        a->propy = 150;  
+        ft_pos2(a);  
     }
     else
     {
         ft_pos(a);
-        a->mul = 4;
     }
 }
 
 int	ft_expose_hook(t_env *a)
 {
+    mlx_destroy_image(a->mlx, a->img);
 	a->img = mlx_new_image(a->mlx, a->win_width, a->win_length);
 	a->imgstr = (int*)mlx_get_data_addr(a->img, &a->bpp, &a->sl, &a->endian);
+    ft_bzero(&a->point, sizeof(t_point));
 	drawcoord(a);
-	mlx_destroy_image(a->mlx, a->img);
 	return (0);
 }
 
@@ -305,72 +361,75 @@ void	key_proj(t_env *fdf)
 void key_move2(int keycode, t_env *a)
 {
 
-    if (keycode == 125)
+    if (keycode == 125 && a->point.ymax < a->win_length)
     {
-        a->propy += 20;
+        a->propy += a->prop;
     }
-    else if (keycode == 126)
+    else if (keycode == 126 && a->point.ymin > 50)
     {
-        a->propy -= 20;
+        a->propy -= a->prop;
+        if (a->propy < 0)
+            a->propy = 0;
     }
-    else if (keycode == 124)
+    else if (keycode == 124 && a->point.xmax < a->win_width)
     {
-        a->propx += 20;
+        a->propx += a->prop;
   
     }
-    else if (keycode == 123)
+    else if (keycode == 123 && a->point.xmin > 50)
     {
-        a->propx -= 20;
+        a->propx -= a->prop;
+        if (a->propx < 0)
+            a->propx = 0;
     }  
 }
 void key_move(int keycode, t_env *a)
 {
     if (a->proj == 1)
         key_move2(keycode, a);
-    else if (keycode == 125 && (a->x < a->win_width || a->y < a->win_width
-        || a->x > -a->win_width || a->y > -a->win_width))
+    else if (keycode == 125 && a->point.xmax < a->win_width && a->point.ymax < a->win_length)
     {
-        a->propx += 20;
-        a->propy += 20;
+        a->propx += 10;
+        a->propy += 10;
     }
-    else if (keycode == 126 && (a->x < a->win_width || a->y < a->win_width
-        || a->x > -a->win_width || a->y > -a->win_width))
+    else if (keycode == 126 && a->point.xmin > -a->win_width && a->point.ymin > -a->win_length)
     {
-        a->propx -= 20;
-        a->propy -= 20;
+        a->propx -= 10;
+        a->propy -= 10;
     }
-    else if (keycode == 124 &&  (a->x < a->win_width || a->y < a->win_width
-        || a->x > -a->win_width || a->y > -a->win_width))
+    else if (keycode == 124 && a->point.xmax < a->win_width && a->point.ymin > -a->win_length)
     {
-        a->propx += 20;
-        a->propy -= 20;
+        a->propx += 10;
+        a->propy -= 10;
     }
-    else if (keycode == 123 &&  (a->x < a->win_width || a->y < a->win_width
-        || a->x > -a->win_width || a->y > -a->win_width))
+    else if (keycode == 123 && a->point.xmin > -a->win_width && a->point.ymax < a->win_length)
     {
-        a->propx -= 20;
-        a->propy += 20;
+        a->propx -= 10;
+        a->propy += 10;
     }
-    printf("x = %d\n", a->propx);
-    printf("y = %d\n", a->propy);
     printf("width = %d\n", a->win_width);
     printf("length = %d\n", a->win_length);
+    printf(" xmax = %d\n", a->point.xmax);
+    printf("ymax = %d\n", a->point.ymax);
+    printf(" xmin = %d\n", a->point.xmin);
+    printf("ymin = %d\n", a->point.ymin);
+    printf("prop = %d\n", a->prop);
 }
 
 void key_alt(int keycode, t_env *a)
 {
-    if (keycode == 12 &&  (a->x < a->win_width || a->y < a->win_width
-        || a->x > -a->win_width || a->y  > -a->win_width))
+    if (keycode == 12 &&  (a->point.x < a->win_width || a->point.y < a->win_width
+        || a->point.x > -a->win_width || a->point.y  > -a->win_width))
         a->mul++;
-    if (keycode == 13 &&  (a->x < a->win_width || a->y < a->win_width
-        || a->x >= -a->win_width || a->y > -a->win_width))
+    if (keycode == 13 &&  (a->point.x < a->win_width || a->point.y < a->win_width
+        || a->point.x >= -a->win_width || a->point.y > -a->win_width))
         a->mul--;
 }
 
 void key_zoom(int keycode, t_env *a)
 {
-    if (keycode == 24 && (a->x < a->win_width || a->y < a->win_width
-        || a->x <= -a->win_width || a->y <= -a->win_width))
+    if (keycode == 24 && (a->point.x < a->win_width || a->point.y < a->win_width
+        || a->point.x <= -a->win_width || a->point.y <= -a->win_width))
     {
         a->prop += 5;
     }
@@ -380,8 +439,6 @@ void key_zoom(int keycode, t_env *a)
         if (a->prop <= 0)
             a->prop = 5;
     }
-    printf(" x = %d\n", a->x);
-    printf("y = %d\n", a->y);
 }
 
 int key_funct(int keycode, t_env *a)
@@ -410,6 +467,7 @@ int key_funct(int keycode, t_env *a)
 void mlx(t_env *a)
 {
     a->proj = 0;
+    a->mul = 4;
     init(a);
     ft_space(a);
     ft_window(a);
